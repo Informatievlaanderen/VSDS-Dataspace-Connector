1f5a8e1135d66b1797246eb6e50aa3fd6b7ccbda
Tom Van Besien <tom.vanbesien@gmail.com>
2023-10-30 10:15:54 +0100

    Init commit

---
+++ http-pull-connector/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    `java-library`
+    id("application")
+    alias(libs.plugins.shadow)
+}
+
+group = "be.vlaanderen.informatievlaanderen.ldes"
+version = "unspecified"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+    implementation(libs.edc.control.plane.core)
+    implementation(libs.edc.dsp)
+    implementation(libs.edc.configuration.filesystem)
+    implementation(libs.edc.vault.filesystem)
+    implementation(libs.edc.iam.mock)
+    implementation(libs.edc.management.api)
+    implementation(libs.edc.transfer.data.plane)
+    implementation(libs.edc.transfer.pull.http.receiver)
+
+    implementation(libs.edc.data.plane.selector.api)
+    implementation(libs.edc.data.plane.selector.core)
+    implementation(libs.edc.data.plane.selector.client)
+
+    implementation(libs.edc.data.plane.api)
+    implementation(libs.edc.data.plane.core)
+    implementation(libs.edc.data.plane.http)
+
+}
+
+application {
+    mainClass.set("$group.boot.system.runtime.BaseRuntime")
+}
+
+var distTar = tasks.getByName("distTar")
+var distZip = tasks.getByName("distZip")
+
+tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
+    mergeServiceFiles()
+    archiveFileName.set("pull-connector.jar")
+    dependsOn(distTar, distZip)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+rootProject.name = "vsds-dataspace-connector"
+
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        mavenLocal()
+    }
+}
+
+include("http-pull-connector")
